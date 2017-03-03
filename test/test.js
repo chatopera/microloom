7c92a0f8df18c27366a33a24d037abec4df18eed
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-03-03 23:15:09 +0800

    support async in middlewares

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ function wait(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms || 1))
 }
 
-test.cb('Microloom#yield', t => {
+test.only.cb('Microloom#yield', t => {
     app.use(function* (ctx, next) {
         ctx.arr.push(1)
         yield next()
@@ -21,6 +21,34 @@ test.cb('Microloom#yield', t => {
     app.use(function* (ctx, next) {
         ctx.arr.push(2)
         yield next()
+        ctx.arr.push(3)
+    });
+
+    app.handle({
+        arr: [0]
+    }).then(function (result) {
+        console.log(result);
+        t.is(result.arr[0], 0, 'arr[0] does not match');
+        t.is(result.arr[1], 1, 'arr[1] does not match');
+        t.pass();
+        t.end();
+    }).catch(function (e) {
+        console.error(e)
+    });
+});
+
+test.cb('Microloom#async', t => {
+    app.use(async function* (ctx, next) {
+        ctx.arr.push(1)
+        await next()
+        await wait(1)
+        ctx.arr.push(4)
+        return ctx;
+    });
+
+    app.use(async function* (ctx, next) {
+        ctx.arr.push(2)
+        await next()
         ctx.arr.push(3)
     });
 
