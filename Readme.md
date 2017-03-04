2ce29add6e542c434d760cfdc822473e0e1c49e8
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-03-04 10:21:56 +0800

    update Readme.md

--- Readme.md
+++ Readme.md
@@ -3,14 +3,15 @@
 **Microloom** requires node v7.6.0 or higher for ES2015 and async function support.
 
 ## Install
-
 ```
 npm install microloom --save
 ```
+
 ## Usage
+Check out [Example](https://github.com/Samurais/microloom/tree/master/test/test.js).
 
 ```
-    app.use(async function* (ctx, next) {
+    app.use(async function (ctx, next) {
         ctx.arr.push(1)
         await next()
         await wait(1)
@@ -18,7 +19,7 @@ npm install microloom --save
         return ctx;
     });
 
-    app.use(async function* (ctx, next) {
+    app.use(async function (ctx, next) {
         ctx.arr.push(2)
         await next()
         ctx.arr.push(3)
@@ -31,6 +32,14 @@ npm install microloom --save
     }).catch(function (e) {
         console.error(e)
     });
+```
+
+## Contribution
+```
+git clone git@github.com:Samurais/microloom.git
+cd microloom
+npm install 
+ava
 ```
 
 ## License 
--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ function wait(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms || 1))
 }
 
-test.only.cb('Microloom#yield', t => {
+test.skip.cb('Microloom#yield', t => {
     app.use(function* (ctx, next) {
         ctx.arr.push(1)
         yield next()
@@ -37,8 +37,8 @@ test.only.cb('Microloom#yield', t => {
     });
 });
 
-test.cb('Microloom#async', t => {
-    app.use(async function* (ctx, next) {
+test.skip.cb('Microloom#async', t => {
+    app.use(async function (ctx, next) {
         ctx.arr.push(1)
         await next()
         await wait(1)
@@ -46,7 +46,7 @@ test.cb('Microloom#async', t => {
         return ctx;
     });
 
-    app.use(async function* (ctx, next) {
+    app.use(async function (ctx, next) {
         ctx.arr.push(2)
         await next()
         ctx.arr.push(3)
@@ -55,7 +55,7 @@ test.cb('Microloom#async', t => {
     app.handle({
         arr: [0]
     }).then(function (result) {
-        console.log(result);
+        console.log('result', result);
         t.is(result.arr[0], 0, 'arr[0] does not match');
         t.is(result.arr[1], 1, 'arr[1] does not match');
         t.pass();
@@ -64,3 +64,75 @@ test.cb('Microloom#async', t => {
         console.error(e)
     });
 });
+
+
+test.only.cb('Microloom#check next', t => {
+    app.use(async function (ctx, next) {
+        ctx.arr.push(1)
+        await next()
+        await wait(1)
+        ctx.arr.push(6)
+        return ctx;
+    });
+
+    app.use(async function (ctx, next) {
+        ctx.arr.push(2)
+        if (ctx.ismw3)
+            await next()
+        ctx.arr.push(5)
+    });
+
+    app.use(async function (ctx, next) {
+        ctx.arr.push(3)
+        ctx.arr.push(4)
+    });
+
+    app.handle({
+        ismw3: false,
+        arr: [0]
+    }).then(function (result) {
+        console.log('result', result);
+        t.is(result.arr[0], 0, 'arr[0] does not match');
+        t.is(result.arr[1], 1, 'arr[1] does not match');
+        t.pass();
+        t.end();
+    }).catch(function (e) {
+        console.error(e)
+    });
+})
+
+
+test.only.cb('Microloom#check next 2', t => {
+    app.use(async function (ctx, next) {
+        ctx.arr.push(1)
+        await next()
+        await wait(1)
+        ctx.arr.push(6)
+        return ctx;
+    });
+
+    app.use(async function (ctx, next) {
+        ctx.arr.push(2)
+        if (ctx.ismw3)
+            await next()
+        ctx.arr.push(5)
+    });
+
+    app.use(async function (ctx, next) {
+        ctx.arr.push(3)
+        ctx.arr.push(4)
+    });
+
+    app.handle({
+        ismw3: true,
+        arr: [0]
+    }).then(function (result) {
+        console.log('result', result);
+        t.is(result.arr[0], 0, 'arr[0] does not match');
+        t.is(result.arr[1], 1, 'arr[1] does not match');
+        t.pass();
+        t.end();
+    }).catch(function (e) {
+        console.error(e)
+    });
+})
